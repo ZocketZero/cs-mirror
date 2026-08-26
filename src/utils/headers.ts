9082fa229ec148b3ca8a9d66f0a5b1890ab91af0
Nawasan Wisitsingkhon <zocketzero@proton.me>
2026-08-27 02:03:12 +0700

    remove x- headers from vercel

--- src/utils/headers.ts
+++ src/utils/headers.ts
@@ -1,15 +1,26 @@
 import yaml from "js-yaml";
 import { Elysia } from "elysia";
 
+function stripXHeaders(obj: { [key: string]: string }, caseInsensitive = true) {
+  return Object.fromEntries(
+    Object.entries(obj).filter(([key]) =>
+      caseInsensitive
+        ? !key.toLowerCase().startsWith("x-")
+        : !key.startsWith("x-")
+    )
+  );
+}
+
 export function getHeaderValue(
   headers: { [key: string]: string },
   id: string | undefined,
   format: string | undefined
 ) {
-  if (format === "yaml" || id === ".yaml") return yaml.dump({ headers });
+  const headers_filter = stripXHeaders(headers);
+  if (format === "yaml" || id === ".yaml") return yaml.dump({ headers: headers_filter });
   else if (id !== undefined && id !== ".json")
     return { message: "this format is not avaliable" };
-  else return { headers };
+  else return { headers: headers_filter };
 }
 
 export const headersRoute = new Elysia()
